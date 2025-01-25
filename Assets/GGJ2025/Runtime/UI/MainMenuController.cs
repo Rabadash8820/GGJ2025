e06de68b0b4feafe61e6683b677837ca70dc0231
Dan Vicarel <dan.vicarel@gmail.com>
2025-01-25 00:35:55 -0500

    Set root namespace to `GGJ2025` and move existing scripts into that namespace

--- Assets/GGJ2025/Runtime/UI/MainMenuController.cs
+++ Assets/GGJ2025/Runtime/UI/MainMenuController.cs
@@ -3,38 +3,41 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UIElements;
 
-public class MainMenuController : MonoBehaviour
+namespace GGJ2025.UI
 {
-    [RequiredIn(PrefabKind.NonPrefabInstance)]
-    public UIDocument UIDocument;
-
-    public string BtnPlayName = "btn-play";
-    public string BtnCreditsName = "btn-credits";
-    public string BtnQuitName = "btn-quit";
-
-    public UnityEvent PlaySelected = new();
-    public UnityEvent CreditsSelected = new();
-    public UnityEvent QuitSelected = new();
-
-    private void Awake() => setupButtons(UIDocument.rootVisualElement);
-
-    private void setupButtons(VisualElement root)
+    public class MainMenuController : MonoBehaviour
     {
-        Button btnPlayName = root.Query<Button>(BtnPlayName).First();
-        Button btnCreditsName = root.Query<Button>(BtnCreditsName).First();
-        Button btnQuitName = root.Query<Button>(BtnQuitName).First();
+        [RequiredIn(PrefabKind.NonPrefabInstance)]
+        public UIDocument UIDocument;
 
-        btnPlayName.clicked += () => {
-            Debug.Log("Play button clicked");
-            PlaySelected.Invoke();
-        };
-        btnCreditsName.clicked += () => {
-            Debug.Log("Credits button clicked");
-            CreditsSelected.Invoke();
-        };
-        btnQuitName.clicked += () => {
-            Debug.Log("Quit button clicked");
-            QuitSelected.Invoke();
-        };
+        public string BtnPlayName = "btn-play";
+        public string BtnCreditsName = "btn-credits";
+        public string BtnQuitName = "btn-quit";
+
+        public UnityEvent PlaySelected = new();
+        public UnityEvent CreditsSelected = new();
+        public UnityEvent QuitSelected = new();
+
+        private void Awake() => setupButtons(UIDocument.rootVisualElement);
+
+        private void setupButtons(VisualElement root)
+        {
+            Button btnPlayName = root.Query<Button>(BtnPlayName).First();
+            Button btnCreditsName = root.Query<Button>(BtnCreditsName).First();
+            Button btnQuitName = root.Query<Button>(BtnQuitName).First();
+
+            btnPlayName.clicked += () => {
+                Debug.Log("Play button clicked");
+                PlaySelected.Invoke();
+            };
+            btnCreditsName.clicked += () => {
+                Debug.Log("Credits button clicked");
+                CreditsSelected.Invoke();
+            };
+            btnQuitName.clicked += () => {
+                Debug.Log("Quit button clicked");
+                QuitSelected.Invoke();
+            };
+        }
     }
 }
