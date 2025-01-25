f979f983a32632f17e2fc648178d4be6ae3207a1
Dan Vicarel <dan.vicarel@gmail.com>
2025-01-25 17:04:39 -0500

    Add ability to collect bubbles to player

---
+++ Assets/GGJ2025/Runtime/BubbleCollector.cs
@@ -0,0 +1,46 @@
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace GGJ2025
+{
+    [RequireComponent(typeof(SphereCollider))]
+    public class BubbleCollector : MonoBehaviour
+    {
+        private const float PI_X4 = Mathf.PI * 4f;
+        private const float TRIGGER_OFFSET = 0.02f;
+
+        private SphereCollider _sphereCollider;
+
+        [RequiredIn(PrefabKind.NonPrefabInstance)]
+        public GameObject ScaledBubbleObject;
+
+        public UnityEvent BubbleCollected = new();
+
+        private void Awake() => _sphereCollider = GetComponent<SphereCollider>();
+
+        private void OnTriggerEnter(Collider other)
+        {
+            Transform collectedParentTransform = other.attachedRigidbody.transform;
+            CollectibleBubble collectibleBubble = collectedParentTransform.GetComponentInChildren<CollectibleBubble>();
+            if (collectibleBubble == null)
+                return;
+
+            float oldBubbleRadius = _sphereCollider.radius - TRIGGER_OFFSET;
+            float collectedSurfaceArea = getSphereSurfaceArea(collectibleBubble.SphereCollider.radius);
+            float currBubbleSurfaceArea = getSphereSurfaceArea(oldBubbleRadius);
+            float newBubbleRadius = Mathf.Sqrt((currBubbleSurfaceArea + collectedSurfaceArea) / PI_X4);
+            _sphereCollider.radius = newBubbleRadius + TRIGGER_OFFSET;
+
+            ScaledBubbleObject.transform.localScale = ScaledBubbleObject.transform.localScale * (newBubbleRadius / oldBubbleRadius);
+
+            Debug.Log($"Collected bubble '{collectedParentTransform.name}' with radius {collectibleBubble.SphereCollider.radius}, new radius: {newBubbleRadius}");
+
+            collectibleBubble.Collect();
+
+            BubbleCollected.Invoke();
+        }
+
+        private float getSphereSurfaceArea(float radius) => PI_X4 * radius * radius;
+    }
+}
