62ab8a0ffe0029cc085640020b3715c451ed161f
Dan Vicarel <dan.vicarel@gmail.com>
2025-01-26 02:00:33 -0500

    Person heads can now only be hit once

--- Assets/GGJ2025/Runtime/PersonHead.cs
+++ Assets/GGJ2025/Runtime/PersonHead.cs
@@ -1,9 +1,16 @@
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace GGJ2025
 {
     public class PersonHead : MonoBehaviour
     {
+        [ShowInInspector, ReadOnly]
+        public bool IsHit { get; private set; }
 
+        public void Hit()
+        {
+            IsHit = true;
+        }
     }
 }
