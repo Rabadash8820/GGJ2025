231cd24fa4a2d236bb4ef62743e460a67545452e
Dan Vicarel <dan.vicarel@gmail.com>
2025-01-25 20:55:54 -0500

    Show how many people are hit in HUD, and raise an event when all are hit

--- Assets/GGJ2025/Runtime/UI/HudController.cs
+++ Assets/GGJ2025/Runtime/UI/HudController.cs
@@ -7,19 +7,27 @@ namespace GGJ2025
     public class HudController : MonoBehaviour
     {
         private TextElement _lblScore;
+        private TextElement _lblPeopleHit;
 
         [RequiredIn(PrefabKind.NonPrefabInstance)]
         public UIDocument UIDocument;
 
         public string LblScoreName = "lbl-score";
+        public string LblPeopleHitName = "lbl-people-hit";
         public string ScoreFormatString = "Score: {0}";
+        public string PeopleHitFormatString = "People hit: {0}";
 
         private void Awake()
         {
             VisualElement root = UIDocument.rootVisualElement;
             _lblScore = root.Query<TextElement>(LblScoreName).First();
+            _lblPeopleHit = root.Query<TextElement>(LblPeopleHitName).First();
         }
 
-        public void UpdateScore(float score) => _lblScore.text = string.Format(ScoreFormatString, score);
+        public void UpdateScore(int peopleHit, int score)
+        {
+            _lblScore.text = string.Format(ScoreFormatString, score);
+            _lblPeopleHit.text = string.Format(PeopleHitFormatString, peopleHit);
+        }
     }
 }
